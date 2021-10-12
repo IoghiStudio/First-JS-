4bab178825d9e7702fc271f779efdf5a3d8f232c
Nicu Iorga <88979813+IoghiStudio@users.noreply.github.com>
2021-10-12 05:32:57 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+var number = prompt('Guess a lucky number between 1-10 and win 100$')
+if (number ==='1' , '2' , '3' , '4' , '5' , '6' , '7', '8' , '9' , '10') {
+    alert('You won 100$!');
+} 
---
+++ script2.js
@@ -0,0 +1,5 @@
+var rezultat = prompt('You tough is that easy? Haha , there is one more question! 185-99 ? ') 
+if (rezultat === '86') {
+    alert('Congratulation!');
+} else { alert('Wrong number!');
+}
